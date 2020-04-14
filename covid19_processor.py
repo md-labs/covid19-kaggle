ff76247fa78f40b63dbaf9653b9e4c862c1e78b8
Rishab Banerjee <crpbanerjee@gmail.com>
2020-04-13 23:23:09 -0700

    code for 3 models: 1. BertModel forward pass to get embeddings; 2. BioBert forward pass to get embeddings; 3. BertForSequenceClassification finetuning to get embeddings

---
+++ covid19_processor.py
@@ -0,0 +1,200 @@
+import glob
+import json
+import logging
+import os
+from collections import defaultdict
+import pickle
+
+
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+  format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+  datefmt="%m/%d/%Y %H:%M:%S",
+  level=logging.INFO)
+
+
+class PaperAbstractDataset(Dataset):
+  """
+  returns the paper_id np array, input_ids tensor and attention_mask tensor
+  """
+  def __init__(self, paper_ids, input_ids, attention_masks, labels):
+    self.input_ids = input_ids
+    self.attention_masks = attention_masks
+    self.labels = labels
+    self.paper_ids = paper_ids
+
+  def __getitem__(self, index):
+    return self.paper_ids[index], self.input_ids[index], self.attention_masks[index], self.labels[index]
+
+  def __len__(self):
+    assert len(self.paper_ids) == self.input_ids.shape[0] == self.attention_masks.shape[0] == len(self.labels)
+    return len(self.paper_ids)
+
+
+class Covid19Processor(object):
+  def __init__(self):
+    pass
+
+  def get_vaccine_and_therapeutic_paper_ids(self, filter_files_dir, only_vaccine_file, only_therapeutic_file):
+    """
+    only_vaccine_file: a file containing a list of paper ids that only contain vaccine keywords
+    only_therapeutic_file: a file containing a list of paper ids that only contain therapeutic keywords
+    return set of vaccine only paper_ids and therapeutic only paper_ids
+    """
+    only_vaccine_file_names = set()
+    only_therapeutic_file_names = set()
+
+    with open(os.path.join(filter_files_dir, only_vaccine_file), "r") as f:
+      only_vaccine_file_names.update(set([paper_id.rstrip("\n") for paper_id in f.readlines()]))
+    with open(os.path.join(filter_files_dir, only_therapeutic_file), "r") as f:
+      only_therapeutic_file_names.update(set([paper_id.rstrip("\n") for paper_id in f.readlines()]))
+    return only_vaccine_file_names, only_therapeutic_file_names
+
+  def extract_json_file_names(self, data_dir, filter_dir):
+    json_file_names = glob.glob(f"{data_dir}/biorxiv_medrxiv/pdf_json/*.json")
+    json_file_names.extend(glob.glob(f"{data_dir}/comm_use_subset/pdf_json/*.json"))
+    json_file_names.extend(glob.glob(f"{data_dir}/comm_use_subset/pmc_json/*.json"))
+    json_file_names.extend(glob.glob(f"{data_dir}/custom_license/pdf_json/*.json"))
+    json_file_names.extend(glob.glob(f"{data_dir}/custom_license/pmc_json/*.json"))
+    json_file_names.extend(glob.glob(f"{data_dir}/noncomm_use_subset/pdf_json/*.json"))
+    json_file_names.extend(glob.glob(f"{data_dir}/noncomm_use_subset/pmc_json/*.json"))
+
+    if filter_dir is not None:
+      filter_file_names = glob.glob(f"{filter_dir}/*.out")
+      filter_set = set()
+      for filter_file_name in filter_file_names:
+        with open(filter_file_name, "r") as f:
+          filter_set.update(set([paper_id.rstrip("\n") + ".json" for paper_id in f.readlines()]))
+
+      json_file_names = list(filter(lambda x: os.path.split(x)[1] in filter_set, json_file_names))
+
+    logger.info(" sample json file name: %s", str(json_file_names[0]))
+    logger.info(" total json files available in dataset: %d", len(json_file_names))
+    return json_file_names
+
+  def preprocess_data_to_df(self, json_files, only_vaccine_paper_ids, only_therapeutic_paper_ids):
+    print("only_vaccine_paper_ids")
+    print(only_vaccine_paper_ids)
+    print("\n\n")
+    print("only_vaccine_paper_ids")
+    print(only_vaccine_paper_ids)
+    print("\n\n")
+
+    empty_abstract_file_names = []
+    empty_body_file_names = []
+    paper_data = defaultdict(lambda: defaultdict(list))
+
+    stale_keys = set()
+
+    for json_file_name in json_files:
+      with open(json_file_name) as f:
+        json_file = json.load(f)
+
+        paper_id = json_file["paper_id"]
+
+        # populate the body_text
+        if json_file["body_text"] == []:
+          empty_body_file_names.append(paper_id)
+        else:
+          for point in json_file["body_text"]:
+            paper_data[paper_id]["body_text"].append(point["text"])
+
+        # abstract
+        if "abstract" not in json_file:
+          empty_abstract_file_names.append(paper_id)
+          stale_keys.add(tuple(json_file.keys()))
+          continue
+
+        # populate the abstract
+        if json_file["abstract"] == []:
+          empty_abstract_file_names.append(paper_id)
+        else:
+          for point in json_file["abstract"]:
+            paper_data[paper_id]["abstract"].append(point["text"])
+
+    data = []
+    labels = []
+    valid_abstracts = 0
+    print("total papers:", len(paper_data.keys()))
+    for idx, paper_id in enumerate(paper_data.keys()):
+      paper_data[paper_id]["body_text"] = "".join(paper_data[paper_id]["body_text"])
+      if "abstract" in paper_data[paper_id]:
+        paper_data[paper_id]["abstract"] = "".join(paper_data[paper_id]["abstract"])
+      else:
+        paper_data[paper_id]["abstract"] = ""
+
+      # if "abstract" in paper_data[paper_id] and detect(paper_data[paper_id]["abstract"]) is "en":
+      if len(paper_data[paper_id]["abstract"]) >= 50 and \
+              ("cov" in paper_data[paper_id]["body_text"] or
+               "COV" in paper_data[paper_id]["body_text"] or
+               "Cov" in paper_data[paper_id]["body_text"] or
+               "CoV" in paper_data[paper_id]["body_text"]):
+        valid_abstracts += 1
+      data.append((paper_id,
+                   paper_data[paper_id]["abstract"],
+                   paper_data[paper_id]["body_text"]))
+      labels.append(0 if paper_id in only_vaccine_paper_ids else 1)
+
+    logger.info(" total valid abstracts: %s", valid_abstracts)
+    logger.info(" empty_abstract_file_names: %d", len(empty_abstract_file_names))
+    logger.info(" empty_body_file_names %d", len(empty_body_file_names))
+    data = pd.DataFrame(data, columns=["paper_id", "abstract", "body_text"])
+    labels = pd.DataFrame(labels, columns=["label"])
+    logger.info(" shape of data: %s", str(data.shape))
+    return train_test_split(data, labels, test_size=0.33)
+
+  def create_input_ids__attention_masks_tensor(self, X_train, y_train, tokenizer, max_seq_length):
+    # Tokenize all of the sentences and map the tokens to their word IDs.
+    abstracts = X_train["abstract"].values
+    paper_ids = X_train["paper_id"].values
+    labels = y_train["label"].values
+
+    paper_ids_with_abstracts = []
+    labels_with_abstract = []
+    input_ids = []
+    attention_masks = []
+
+    for idx, (paper_id, point, label) in enumerate(zip(paper_ids, abstracts, labels)):
+      if len(point) == 0:
+        continue
+
+      # `encode_plus` will:
+      #   (1) Tokenize the sentence.
+      #   (2) Prepend the `[CLS]` token to the start.
+      #   (3) Append the `[SEP]` token to the end.
+      #   (4) Map tokens to their IDs.
+      #   (5) Pad or truncate the sentence to `max_length`
+      #   (6) Create attention masks for [PAD] tokens.
+      encoded_dict = tokenizer.encode_plus(
+        point,  # Sentence to encode.
+        add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+        max_length=max_seq_length,  # Pad & truncate all sentences.
+        pad_to_max_length=True,
+        return_attention_mask=True,  # Construct attn. masks.
+        return_tensors='pt',  # Return pytorch tensors.
+      )
+
+      # Add the encoded sentence to the list.
+      input_ids.append(encoded_dict['input_ids'])
+
+      # And its attention mask (simply differentiates padding from non-padding).
+      attention_masks.append(encoded_dict['attention_mask'])
+
+      paper_ids_with_abstracts.append(paper_id)
+      labels_with_abstract.append(label)
+
+    # Convert the lists into tensors.
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+    torch.save(input_ids, 'input_ids.pt')
+    torch.save(attention_masks, 'attention_masks.pt')
+    with open(f'paper_ids.pickle', 'wb') as handle:
+      pickle.dump(paper_ids_with_abstracts, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(f'labels.pickle', 'wb') as handle:
+      pickle.dump(labels_with_abstract, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    return input_ids, attention_masks, paper_ids_with_abstracts, labels_with_abstract
---
+++ find_document_similarity.py
@@ -0,0 +1,205 @@
+"""
+Gives the document scores based on how much of their abstracts is similar to the words similar to seed words.
+
+1. Read the tokens (that are similar to the seed words) and their embeddings from "similar_words/"
+2. Read the input_ids generated from covid-embeddings-simple-code.py from "inputs/" folder
+3. Read the attention_masks generated from covid-embeddings-simple-code.py from "inputs/" folder
+4. Read the paper_ids of paper with abstracts generated from covid-embeddings-simple-code.py from "inputs/" folder
+5. Writes a dictionary of the format {paper_id: score} in "document_scores/" folder
+
+similar_words/, inputs/ =====>  document_scores/
+"""
+import argparse
+import pickle
+import random
+import time
+import logging
+import datetime
+
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import torch
+from torch.utils.data import Dataset, DataLoader, SequentialSampler
+
+from transformers import BertTokenizer, BertModel, BertForSequenceClassification, BertConfig
+
+
+def format_time(elapsed):
+  '''
+  Takes a time in seconds and returns a string hh:mm:ss
+  '''
+  # Round to the nearest second.
+  elapsed_rounded = int(round((elapsed)))
+  # Format as hh:mm:ss
+  return str(datetime.timedelta(seconds=elapsed_rounded))
+
+
+class PaperAbstractDataset(Dataset):
+  """
+  returns the paper_id np array, input_ids tensor and attention_mask tensor
+  """
+
+  def __init__(self, paper_ids, input_ids, attention_masks):
+    self.paper_ids = paper_ids
+    self.input_ids = input_ids
+    self.attention_masks = attention_masks
+
+  def __getitem__(self, index):
+    paper_id = self.paper_ids[index]
+    input_id = self.input_ids[index]
+    attention_mask = self.attention_masks[index]
+
+    return paper_id, input_id, attention_mask
+
+  def __len__(self):
+    assert len(self.paper_ids) == self.input_ids.shape[0] == self.attention_masks.shape[0]
+    return self.input_ids.shape[0]
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+  format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+  datefmt="%m/%d/%Y %H:%M:%S",
+  level=logging.INFO)
+
+
+def main():
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument(
+    "--similar_tokens_to_embeddings",
+    default="closest_word_to_embeddings_whole_dataset_biobert",
+    type=str,
+    required=False,
+    help="The .pickle file which stores the map {token_similar_to_seed: embedding}.")
+
+  parser.add_argument(
+    "--data_dir",
+    default="whole_dataset_biobert",
+    type=str,
+    required=False,
+    help="The directory storing the input_ids.pt, attention_masks.pt and paper_ids.")
+
+  parser.add_argument(
+    "--model_path",
+    default=None,
+    type=str,
+    required=False,
+    help="The path to the .bin transformer model.")
+
+  parser.add_argument(
+    "--out_dir",
+    default="whole_dataset_biobert",
+    type=str,
+    required=True,
+    help="The directory storing the word embeddings of the tokens (as python dictionary {token : embedding}) in pickle format")
+
+  parser.add_argument(
+    "--batch_size",
+    default=16,
+    type=int,
+    help="The batch size to feed the model")
+
+  parser.add_argument(
+    "--top_k",
+    default=20,
+    type=int,
+    help="Only the <top_k> tokens in every abstract are used for measuring an abstracts similarity value.")
+
+  args = parser.parse_args()
+
+  with open(f"similar_words/{args.similar_tokens_to_embeddings}.pickle", "rb") as f:
+    similar_token_to_embedding = np.stack(list(pickle.load(f).values())[:-1])  # np.stack is needed as the contents of the pickle file is funny, print and see
+
+  input_ids = torch.load(f"inputs/{args.data_dir}/input_ids.pt")
+  attention_masks = torch.load(f"inputs/{args.data_dir}/attention_masks.pt")
+  with open(f"inputs/{args.data_dir}/paper_ids.pickle", "rb") as f:
+    paper_ids = pickle.load(f)
+
+  logger.info("%s", str(input_ids.shape))
+  logger.info('Token IDs: %s', str(input_ids[0]))
+
+  if args.model_path is None:
+    model = BertModel.from_pretrained("bert-base-cased")
+  else:
+    configuration = BertConfig.from_json_file(f"{args.model_path}/bert_config.json")
+    model = BertModel.from_pretrained(f"{args.model_path}/pytorch_model.bin", config=configuration)
+  model.cuda()
+
+  dataset = PaperAbstractDataset(paper_ids, input_ids, attention_masks)
+
+  batch_size = args.batch_size
+
+  dataloader = DataLoader(
+    dataset,
+    sampler=SequentialSampler(dataset),
+    batch_size=batch_size)
+
+  device = torch.device("cuda")
+  seed_val = 42
+
+  random.seed(seed_val)
+  np.random.seed(seed_val)
+  torch.manual_seed(seed_val)
+  torch.cuda.manual_seed_all(seed_val)
+
+  # Measure the total training time for the whole run.
+  total_t0 = time.time()
+  t0 = time.time()
+
+  logger.info("")
+  logger.info('Forward pass...')
+
+  model.eval()
+
+  paper_ids_to_cosine_score = {}
+  for step, batch in enumerate(dataloader):
+    if step % 100 == 0:
+      logger.info('======== Batch {:} / {:} ========'.format(step, len(dataloader)))
+      logger.info("Time to find embeddings for batches {} to {}: {:} (h:mm:ss)".format(max(0, step - 100), step, format_time(time.time() - t0)))
+      t0 = time.time()
+    # `batch` contains two pytorch tensors and 1 numpy array:
+    #   [0]: paper ids
+    #   [1]: input ids
+    #   [2]: attention masks
+    paper_ids_np = np.array(batch[0], dtype=str)
+    b_input_ids = batch[1].to(device)
+    b_input_mask = batch[2].to(device)
+
+    embeddings, cls = model(b_input_ids,
+                            token_type_ids=None,
+                            attention_mask=b_input_mask)
+
+    # move everything to cpu to save GPU space
+    b_input_ids_np = b_input_ids.cpu().numpy()
+    b_input_mask_np = b_input_mask.cpu().numpy()
+    embeddings_np = embeddings.detach().cpu().numpy()
+    cls_np = cls.detach().cpu().numpy()
+
+    del b_input_ids
+    del b_input_mask
+    del embeddings
+    del cls
+    torch.cuda.empty_cache()
+
+    for batch_number in range(len(embeddings_np)):
+      abstract_cosine_score = np.average(np.sort(cosine_similarity(embeddings_np[batch_number], similar_token_to_embedding))[:args.top_k])
+      paper_id = paper_ids_np[batch_number]
+      paper_ids_to_cosine_score[paper_id] = abstract_cosine_score
+
+    del b_input_ids_np
+    del b_input_mask_np
+    del embeddings_np
+    del cls_np
+
+  with open(f"document_scores/{args.out_dir}.pickle", "wb") as f:
+    pickle.dump(paper_ids_to_cosine_score, f, pickle.HIGHEST_PROTOCOL)
+
+  logger.info("Total time to complete the entire process: {:} (h:mm:ss)".format(format_time(time.time() - total_t0)))
+
+  logger.info("\n")
+  logger.info("Document similarity found!")
+
+
+if __name__ == '__main__':
+  main()
